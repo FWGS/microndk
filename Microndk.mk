7f0f9480a2d39b7566f2058fc00bc7623990c4a6
mittorn <mittorn@sibmail.com>
2016-03-09 18:48:19 +0600

    Experimental mingw support

--- Microndk.mk
+++ Microndk.mk
@@ -1,10 +1,30 @@
-MICRONDK_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
+CC ?= gcc
+my-dir := $(shell pwd)
 PROJECT_DIR ?= $(shell pwd)
+MICRONDK_DIR ?= $(dir $(lastword $(MAKEFILE_LIST)))
+
+SYS := $(shell $(CC) -dumpmachine)
+ifneq (, $(findstring android, $(SYS)))
+MICRONDK_OS := android
+endif
+ifneq (, $(findstring linux, $(SYS)))
+MICRONDK_OS := linux
+endif
+ifneq (, $(findstring mingw, $(SYS)))
+MICRONDK_OS := mingw
+PROJECT_DIR := .
+my-dir := .
+override LOCAL_PATH=.
+endif
+
 CLEAR_VARS := $(MICRONDK_DIR)/clear_vars.mk
 XASH3D_CONFIG := $(MICRONDK_DIR)/xash3d_config.mk
-BUILD_SHARED_LIBRARY := $(MICRONDK_DIR)/build-shared.mk
+ifneq ($(MICRONDK_OS),mingw)
+      BUILD_SHARED_LIBRARY := $(MICRONDK_DIR)/build-shared.mk
+else
+    BUILD_SHARED_LIBRARY := $(MICRONDK_DIR)/build-shared-mingw.mk
+endif
 NANOGL_PATH=../nanogl
 SDL_PATH=../SDL2
 XASH_SDL=1
-my-dir := $(shell pwd)
 include $(PROJECT_DIR)/Android.mk
---
+++ build-shared-mingw.mk
@@ -0,0 +1,36 @@
+ARCH_LIBS :=
+ARCH_CFLAGS := -mstackrealign
+LIBS :=
+INCLUDES := $(foreach inc,$(LOCAL_C_INCLUDES),-I$(inc))
+OBJ_FILES := $(LOCAL_SRC_FILES)
+ifeq ($(LOCAL_MODULE),xash)
+OBJ_FILES += $(wildcard $(NANOGL_PATH)/*.cpp)
+LIBS += libSDL2.so -lm
+INCLUDES += -I$(NANOGL_PATH)/GL
+endif
+ifeq ($(TARGET_ARCH_ABI),armeabi-v7a-hard)
+    ARCH_CFLAGS += -I$(MICRONDK_DIR) -D_NDK_MATH_NO_SOFTFP=1 -mhard-float -mfloat-abi=hard
+    ARCH_LIBS += $(MICRONDK_DIR)/libm_hard.a
+endif
+OBJ_FILES := $(OBJ_FILES:.cpp=.o)
+OBJ_FILES := $(OBJ_FILES:.c=.o)
+MICRONDK_TARGET_CFLAGS := $(ARCH_CFLAGS) $(LOCAL_CFLAGS) $(CFLAGS)
+
+LOCAL_MODULE_FILENAME ?= $(LOCAL_MODULE)
+LOCAL_MODULE_FILENAME := $(subst lib,,$(LOCAL_MODULE_FILENAME))
+MODULE_FILE := $(LOCAL_MODULE_FILENAME).dll
+ifeq ($(MODULE_FILE),)
+     MODULE_FILE := project.dll
+endif
+%.o : %.c
+	$(CC) $(MICRONDK_TARGET_CFLAGS) $(LOCAL_CONLYFLAGS) $(INCLUDES) $(DEFINES) -fPIC -c $< -o $@
+
+%.o : %.cpp
+	$(CXX) $(MICRONDK_TARGET_CFLAGS) $(INCLUDES) $(DEFINES) -c $< -o $@
+
+$(MODULE_FILE) : $(OBJ_FILES)
+#	echo $(OBJ_FILES) $(INCLUDES)
+	g++ -static-libgcc -static-libstdc++ -s -shared -Wl,--add-stdcall-alias -o $(MODULE_FILE) $(ARCH_LIBS) $(LDFLAGS) $(OBJ_FILES) $(ARCH_LIBS) $(LIBS) -Wl,--no-warn-mismatch -Wl,--no-undefined
+clean:
+	del $(subst /,\,$(OBJ_FILES))
+.PHONY: depend clean list
