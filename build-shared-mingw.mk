888de885a37dc909519b886a5c14d2319cc57593
mittorn <mittorn@sibmail.com>
2017-02-11 21:26:58 +0000

    Fix debug in mingw

--- build-shared-mingw.mk
+++ build-shared-mingw.mk
@@ -32,7 +32,7 @@ LOCAL_LDLIBS := $(filter-out -llog,$(LOCAL_LDLIBS))
 
 $(MODULE_FILE) : $(OBJ_FILES)
 #	echo $(OBJ_FILES) $(INCLUDES)
-	g++ -static-libgcc -static-libstdc++ -s -shared -Wl,--add-stdcall-alias -o $(MODULE_FILE) $(ARCH_LIBS) $(LDFLAGS) $(OBJ_FILES) $(ARCH_LIBS) $(LIBS) $(LOCAL_LDFLAGS) $(LOCAL_LDLIBS) -Wl,--no-warn-mismatch -Wl,--no-undefined
+	$(CXX) -static-libgcc -static-libstdc++ -shared -Wl,--add-stdcall-alias -o $(MODULE_FILE) $(ARCH_LIBS) $(LDFLAGS) $(OBJ_FILES) $(ARCH_LIBS) $(LIBS) $(LOCAL_LDFLAGS) $(LOCAL_LDLIBS) -Wl,--no-warn-mismatch -Wl,--no-undefined
 clean:
 	del $(subst /,\,$(OBJ_FILES))
 .PHONY: depend clean list
